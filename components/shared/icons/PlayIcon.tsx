422af830ba6f91804d4972b926784f7d33da072c
“Richard” <richardhuaman.dev@gmail.com>
2026-03-24 15:03:33 -0500

    feat(icons): add PlayIcon and WorldCupLeagueIcon SVG components

---
+++ components/shared/icons/PlayIcon.tsx
@@ -0,0 +1,13 @@
+export default function PlayIcon() {
+  return (
+    <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <mask id="mask0_1406_35677" style={{ maskType: "luminance" }} maskUnits="userSpaceOnUse" x="-1" y="-1" width="12" height="12">
+        <path d="M5 9.00032C7.20938 9.00032 9.00032 7.20938 9.00032 5C9.00032 2.79062 7.20938 0.999672 5 0.999672C2.79062 0.999672 0.999672 2.79062 0.999672 5C0.999672 7.20938 2.79062 9.00032 5 9.00032Z" fill="white" stroke="white" strokeWidth="2" strokeLinejoin="round"/>
+        <path d="M4.19962 5.00028V3.61457L5.39972 4.30743L6.59981 5.00028L5.39972 5.69314L4.19962 6.386V5.00028Z" fill="black" stroke="black" strokeWidth="2" strokeLinejoin="round"/>
+      </mask>
+      <g mask="url(#mask0_1406_35677)">
+        <path d="M0 0H10V10H0V0Z" fill="white"/>
+      </g>
+    </svg>
+  );
+}
---
+++ components/shared/icons/WorldCupLeagueIcon.tsx
@@ -0,0 +1,28 @@
+export default function WorldCupLeagueIcon() {
+  return (
+    <svg width="21" height="16" viewBox="0 0 21 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g filter="url(#filter0_d_4048_1557)">
+        <g clipPath="url(#clip0_4048_1557)">
+          <path d="M18 1H2C0.89543 1 0 1.89543 0 3V13C0 14.1046 0.89543 15 2 15H18C19.1046 15 20 14.1046 20 13V3C20 1.89543 19.1046 1 18 1Z" fill="white"/>
+          <path d="M20 1H0V15H20V1Z" fill="#19BFBF"/>
+          <path fillRule="evenodd" clipRule="evenodd" d="M10 13.1333C12.8929 13.1333 15.2381 10.835 15.2381 7.99997C15.2381 5.16491 12.8929 2.86664 10 2.86664C7.10708 2.86664 4.7619 5.16491 4.7619 7.99997C4.7619 10.835 7.10708 13.1333 10 13.1333ZM14.2596 8.46664C14.0828 10.034 13.0258 11.3415 11.5838 11.9039C12.0481 10.977 12.3194 9.76959 12.3716 8.46664H14.2596ZM11.4183 8.46664C11.3488 10.041 10.9293 11.4324 10.2754 12.1914C10.1843 12.1971 10.0925 12.2 10 12.2C9.9075 12.2 9.81568 12.1971 9.72464 12.1914C9.07066 11.4324 8.65121 10.041 8.58168 8.46664H11.4183ZM7.62835 8.46664C7.68059 9.76959 7.95186 10.977 8.4162 11.9039C6.97418 11.3415 5.91725 10.034 5.74044 8.46664H7.62835ZM8.58168 7.53331H11.4183C11.3488 5.95897 10.9293 4.56757 10.2754 3.8085C10.1843 3.80284 10.0925 3.79997 10 3.79997C9.9075 3.79997 9.81568 3.80284 9.72464 3.8085C9.07066 4.56757 8.65121 5.95897 8.58168 7.53331ZM8.41619 4.09609C7.95186 5.02291 7.68059 6.23035 7.62835 7.53331H5.74044C5.91724 5.96589 6.97418 4.65843 8.41619 4.09609ZM14.2596 7.53331H12.3716C12.3194 6.23035 12.0481 5.0229 11.5838 4.09609C13.0258 4.65843 14.0828 5.96589 14.2596 7.53331Z" fill="white"/>
+        </g>
+      </g>
+      <defs>
+        <filter id="filter0_d_4048_1557" x="-1" y="0" width="22" height="16" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+          <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+          <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+          <feOffset/>
+          <feGaussianBlur stdDeviation="0.5"/>
+          <feComposite in2="hardAlpha" operator="out"/>
+          <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.4 0"/>
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_4048_1557"/>
+          <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_4048_1557" result="shape"/>
+        </filter>
+        <clipPath id="clip0_4048_1557">
+          <rect y="1" width="20" height="14" rx="2" fill="white"/>
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
